1ba60d284433705b44a7e13c2ee266dcc251503b
pujansoni <pujansoni1997@gmail.com>
2021-04-08 23:50:41 -0400

    Adding array destructuring example JS

---
+++ ex6_destructuring_array/app.js
@@ -0,0 +1,25 @@
+// faster/easier way to access/unpack variables from arrays
+
+const fruits = ['orange', 'banana', 'lemon'];
+const friends = ['john', 'peter', 'bob', 'anna', 'kelly'];
+
+const fruit1 = fruits[0];
+const fruit2 = fruits[1];
+const fruit3 = fruits[2];
+
+console.log(fruit1, fruit2, fruit3);
+
+const [john, , bob, , kelly, susan] = friends;
+
+console.log(john, bob, kelly, susan);
+
+let first = 'bob';
+let second = 'john';
+
+// let temp = second;
+// second = first;
+// first = temp;
+
+[second, first] = [first, second];
+
+console.log(first, second);
