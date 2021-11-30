6d8748f2b2aad5ad16a051220ce68e9ebd7e2110
pujansoni <pujansoni1997@gmail.com>
2021-11-29 19:43:45 -0500

    React JS

---
+++ React/demo-project/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// Here we are passing two functions to the ReactDOM.render() method. The first argument is the App() function we created in App.js. The second argument is the element id in which the content content of this function(first argument) to be placed
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
