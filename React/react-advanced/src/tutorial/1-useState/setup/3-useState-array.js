a4d2434c0bbe1f832b8a8339d8eb5344be04d05d
pujansoni <pujansoni1997@gmail.com>
2022-01-31 22:46:29 -0500

    Handling objects and arrays in useState React/react-advanced

--- React/react-advanced/src/tutorial/1-useState/setup/3-useState-array.js
+++ React/react-advanced/src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,7 +2,33 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  // Alternative syntax of using the useState function
+  const [people, setPeople] = React.useState(data);
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id)
+    setPeople(newPeople);
+  }
+
+  return (
+    // Here we have used the <> </> syntax which is equivalent to a React Fragment
+    <>
+      {
+        people.map((person) => {
+          const {id, name} = person;
+          return (
+            <div key={id} className="item">
+              <h4>{name}</h4>
+              {/* Here we are associating the remove button to each item in order to remove the corresponding item */}
+              <button onClick={() => removeItem(id)}>remove</button>
+            </div>
+          );
+        })
+      }
+      {/* Here we are emptying the array and clearing all the item */}
+      <button className="btn" onClick={() => setPeople([])}>
+        clear items
+      </button>
+    </>);
 };
 
 export default UseStateArray;
--- React/react-advanced/src/tutorial/1-useState/setup/4-useState-object.js
+++ React/react-advanced/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,36 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  // We can also pass an object as the first argument in the useState React hook
+  const [person, setPerson] = useState({
+    name: "peter", 
+    age: 24, 
+    message: "random message"
+  });
+  // Alternatively, we can also set 3 different state values as shown below
+  // const [name, setName] = useState("peter");
+  // const [age, setAge] = useState(24);
+  // const [message, setMessage] = useState("random message");
+  const changeMessage = () => {
+    // Here we want to change the message than the line below will override the whole object and it will also remove the name and age
+    // setPerson({message: "hello world"});
+    // To overcome this and to be able to just change one element of the object we will use the spread operator as shown below in order to use a new message
+    setPerson({...person, message: "hello world"});
+
+    // Alternatively, instead of using the objects we can also use new variables in order to create 3 different variables under the name, age, and message values
+    // setMessage("hello world");
+  };
+
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
