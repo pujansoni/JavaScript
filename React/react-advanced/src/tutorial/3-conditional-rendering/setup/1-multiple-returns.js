51e2b6461f7b4914e42e0ca5ae39f1a451a45ddb
pujansoni <pujansoni1997@gmail.com>
2022-02-02 13:33:34 -0500

    multiple returns in components React/react-advanced

--- React/react-advanced/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ React/react-advanced/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,6 +1,15 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
+
+// Here a component is just like a normal function which can have multiple returns
+// Here in this example we are just returning an h2 element, but we can return components comprising of other components and whole application can be returned in this way. One use case may be that of a user where in if a user is logged in then we can return the appropriate content or else we can use the other part of the app to display the appropriate content to the user
 const MultipleReturns = () => {
+  const [loading, setLoading] = useState(false);
+
+  if(loading) {
+    return <h2>Loading...</h2>;
+  }
+
   return <h2>multiple returns</h2>;
 };
 
