50aabd5d274162d988233f7cc06144645b081111
pujansoni <pujansoni1997@gmail.com>
2022-02-02 12:47:37 -0500

    useEffect with return for conditional rendering React/react-advanced

--- React/react-advanced/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ React/react-advanced/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -5,8 +5,31 @@ import React, { useState, useEffect } from 'react';
 
 const UseEffectCleanup = () => {
   const [size, setSize] = useState(window.innerWidth);
-  console.log(size);
-  return <h2>useEffect cleanup</h2>;
+  
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    // Here each time we evoke the listener we are triggering the checkSize function which in turn invoke the setSize function which will trigger a re-render.
+    console.log("useEffect"); 
+    window.addEventListener('resize', checkSize);
+    // Here every time we have a useEffect we have an option to return a function as shown below. In this case, we will use the return option to cleanup the function by using the removeEventListener cause if we keep on adding the event listener it will add unnecessary overhead on the application. So before, we add an event listener we will cleanup the old one:
+    return () => {
+      console.log("cleanup");
+      window.removeEventListener('resize', checkSize);
+    }
+    // The cleanup function are useful in conditional rendering
+  });
+
+  console.log("render");
+
+  return (
+    <>
+      <h1>window</h1>
+      <h2>{size} PX</h2>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
