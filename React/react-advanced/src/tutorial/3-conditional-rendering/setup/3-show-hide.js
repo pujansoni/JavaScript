727eae3fa7479b4cd5f8ab696a84cc44161b072d
Pujan Soni <pujansoni1997@gmail.com>
2022-02-09 15:13:10 -0500

    Using cleanup function in useEffect() to show/hide componenets react/react-advanced

--- React/react-advanced/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ React/react-advanced/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,38 @@
 import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <button className="btn" onClick={() => setShow(!show)}>
+        show/hide
+      </button>
+      {/* Here as we can see that we are not limited to just the html elements, we can also use the components in order to toggle with the short-circuit and ternary operators */}
+      {show && <Item />}
+    </>
+  );
 };
+
+// In this example we will toggle the react component
+const Item = () => {
+  // Here we will also set up a side effect which will be cleaned up. We can use the event listener on the window for this
+  // Here we will need to set up a cleanup function cause even if the empty array(in the dependency parameter) saved us before, in this case the event will be called multiple times as we are toggling the whole component
+  const [size, setSize] = useState(window.innerWidth);
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener('resize', checkSize)
+    return () => {
+      window.removeEventListener('resize', checkSize);
+    }
+  }, []);
+  return (
+    <div style={({marginTop: '2rem'})}>
+      <h1>window</h1>
+      <h2>size : </h2>
+    </div>
+  );
+}
 
 export default ShowHide;
