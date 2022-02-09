61902b187772d710898f8730eb3b8889435c12cc
Pujan Soni <pujansoni1997@gmail.com>
2022-02-09 14:46:01 -0500

    short-circuite & ternary operator react/react-advanced

--- React/react-advanced/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ React/react-advanced/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,34 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
+  const [text, setText] = useState("");
+  const [isError, setIsError] = useState(false);
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  // In JSX we always return an expression, so we will generally use the short-circuit evaluation and ternary operator to display something conditionally
+  // We already saw multiple returns but what if we want to show/hide data based on some condition in the returning JSX expression. In this case we will use short-circuit evaluation/ternary operator
+  return (
+    <>
+      {/*
+      Here the line below will display "hello world"
+      <h1>{firstValue}</h1>
+      Here the line below will display "value :"
+      <h1>value : {secondValue}</h1>
+      */}
+      {/*  The line below will give an error
+      {if() {console.log("hello world")}} */}
+      <h1>{text || 'john doe'}</h1>
+      <button className="btn" onClick={() => setIsError(!isError)}>toggle error</button>
+      {/* The && value is generally used to return the element i.e. showing/hiding of an element. In the example given below if we use the first value is true then we will execute the second value which is an h1 tag 
+      {text && <h1>hello world</h1>} */}
+      {isError && <h1>Error...</h1>}
+      {/* We can also use a ternary operator */}
+      {isError ? <p>there is an error...</p> : <div>
+          <h2>there is no error...</h2>
+        </div>}
+    </>
+  );
 };
 
 export default ShortCircuit;
