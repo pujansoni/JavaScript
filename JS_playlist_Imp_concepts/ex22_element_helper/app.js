012a0d9f4cd047df0dea9a394a584db487541b16
Pujan Soni <pujansoni1997@gmail.com>
2022-02-21 23:09:38 -0500

    JS element exists

--- JS_playlist_Imp_concepts/ex22_element_helper/app.js
+++ JS_playlist_Imp_concepts/ex22_element_helper/app.js
@@ -1,5 +1,30 @@
 const heading = document.querySelector(".heading");
-console.log(heading);
+// console.log(heading);
 
 const listItems = document.querySelectorAll(".list-item");
-console.log(listItems);
+// console.log(listItems);
+
+const getElement = (selector, isList) => {
+  // if(isList) {
+  //   const el = [...document.querySelectorAll(selector)];
+  //   if(el.lenght < 1) {
+  //     throw new Error(`Please double check selector: ${selector}`);
+  //   }
+  //   return el;
+  // }
+
+  // const el = document.querySelector(selector);
+  // // Here we are checking if the element exists
+  // if(el) return el;
+  // Refactoring the above code as given below
+  const el = isList ? [...document.querySelectorAll(selector)] : document.querySelector(selector);
+
+  // not a list - exist or not
+  // list - empty or not
+  if(!isList && el) return el;
+  if(isList && !el.length < 1) return el;
+  
+  throw new Error(`Please double check selector: ${selector}`);
+};
+
+console.log(getElement(".list-item"), true);
