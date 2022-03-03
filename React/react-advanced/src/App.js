ba55edc956c877d410cfe00463129ddf49ee3b9e
Pujan Soni <pujansoni1997@gmail.com>
2022-03-03 08:30:09 -0500

    ( Context API / useContext )React/react-advanced

--- React/react-advanced/src/App.js
+++ React/react-advanced/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // Here if there is an index.js in the setup folder then by default it will import the index.js file as that is the default entry point of React JS
 // import Setup from './tutorial//6-useReducer/setup';
-import Setup from './tutorial//7-prop-drilling/setup/1-prop-drilling';
+import Setup from './tutorial//8-useContext/setup/1-context-api';
 
 function App() {
   return (
--- React/react-advanced/src/tutorial/8-useContext/setup/1-context-api.js
+++ React/react-advanced/src/tutorial/8-useContext/setup/1-context-api.js
@@ -3,6 +3,14 @@ import { data } from '../../../data';
 // more components
 // fix - context api, redux (for more complex cases)
 
+// Here we need to pass the removePerson function down to the SinglePerson component using the context API
+const PersonContext = React.createContext();
+
+// Here we have access to two components after the above line is executed i.e. the Provider and the Consumer
+// two components - Provider(works as a distributer), Consumer (We don't use the Consumer)
+// Here ContextAPI is the root component and the return of the root component we have to wrap in PersonContext and the Provider in this case
+
+
 const ContextAPI = () => {
   const [people, setPeople] = useState(data);
   const removePerson = (id) => {
@@ -11,22 +19,26 @@ const ContextAPI = () => {
     });
   };
   return (
-    <>
-      <h3>prop drilling</h3>
-      <List people={people} removePerson={removePerson} />
-    </>
+    // Here for the Provider we have the value prop and we can pass whatever we want. We can also use the useContext to access whatever is there in the value prop
+    // Here for the value prop again we are in the JSX land so first curly braces indicates that we are entering in the JS world and the second curly braces indicates that it's an object
+    <PersonContext.Provider value={{removePerson, people}}>
+      <h3>Context API / useContext</h3>
+      <List />
+    </PersonContext.Provider>
   );
 };
 
-const List = ({ people, removePerson }) => {
+const List = ({ people }) => {
+  // Another way to access data in the Context
+  const mainData = useContext(PersonContext);
+
   return (
     <>
-      {people.map((person) => {
+      {mainData.people.map((person) => {
         return (
           <SinglePerson
             key={person.id}
             {...person}
-            removePerson={removePerson}
           />
         );
       })}
@@ -34,7 +46,12 @@ const List = ({ people, removePerson }) => {
   );
 };
 
-const SinglePerson = ({ id, name, removePerson }) => {
+const SinglePerson = ({ id, name }) => {
+  // Here we need to pass the context that we created i.e. the PersonContext context
+  // Here we have access to the value that is provided in the ContextAPI component under the PersonContext.Provider without having to pass that to the List Component
+  // Here we know that the removePerson is an object so we can destructure it right away
+  const {removePerson} = useContext(PersonContext);
+
   return (
     <div className='item'>
       <h4>{name}</h4>
@@ -44,3 +61,5 @@ const SinglePerson = ({ id, name, removePerson }) => {
 };
 
 export default ContextAPI;
+
+// It's generally feasible to use the context api option to manage prop if we have two or more nested components that are using props
