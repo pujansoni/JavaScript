d4c9d2e06fc88282ca0cbd976111d182c82d2ef5
Pujan Soni <pujansoni1997@gmail.com>
2022-03-03 21:36:35 -0500

    custom hooks useFetch() React/react-advanced

--- React/react-advanced/src/App.js
+++ React/react-advanced/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // Here if there is an index.js in the setup folder then by default it will import the index.js file as that is the default entry point of React JS
 // import Setup from './tutorial//6-useReducer/setup';
-import Setup from './tutorial//8-useContext/setup/1-context-api';
+import Setup from './tutorial//9-custom-hooks/setup/1-fetch-example';
 
 function App() {
   return (
--- React/react-advanced/src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ React/react-advanced/src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,3 +1,21 @@
 import { useState, useEffect } from 'react';
 
-export const useFetch = () => {};
+// Here we can also add the error useState in order to handle any errors that we may encounter while fetching the data
+// Here either the function needs to be a component or it needs to be a custom hook
+export const useFetch = (url) => {
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    const response = await fetch(url);
+    const products = await response.json();
+    setProducts(products);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, [url]);
+
+  return {loading, products};
+};
