11034fda8b30069bb482e5326cb0ae5bc71d5d8b
Pujan Soni <pujansoni1997@gmail.com>
2022-03-05 22:41:05 -0500

    useCallback() function with custom hook React/react-advanced

--- React/react-advanced/src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ React/react-advanced/src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 // Here we can also add the error useState in order to handle any errors that we may encounter while fetching the data
 // Here either the function needs to be a component or it needs to be a custom hook
@@ -6,16 +6,17 @@ export const useFetch = (url) => {
   const [loading, setLoading] = useState(true);
   const [products, setProducts] = useState([]);
 
-  const getProducts = async () => {
+  const getProducts = useCallback(async () => {
     const response = await fetch(url);
     const products = await response.json();
     setProducts(products);
     setLoading(false);
-  };
+  });
 
+  // Here as the getProducts() function is setting the product state value it will be in an infinite loop. To avoid that we have used the useCallback() function to make sure that we run the function from scratch only when the url changes
   useEffect(() => {
     getProducts();
-  }, [url]);
+  }, [url, getProducts]);
 
   return {loading, products};
 };
