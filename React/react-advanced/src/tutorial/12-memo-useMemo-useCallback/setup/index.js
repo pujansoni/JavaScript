9685af1c14f511c67a42968684fb27e53f414f59
Pujan Soni <pujansoni1997@gmail.com>
2022-03-05 22:34:21 -0500

    useCallback and useMemo functions React/react-advanced

--- React/react-advanced/src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ React/react-advanced/src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -9,9 +9,29 @@ const url = 'https://course-api.com/javascript-store-products'
 
 // Please note that react is fast by default
 
+// useMemo will deal with the value just as React.memo will work with the props
+const calculateMostExpensive = (data) => {
+  return data.reduce((total, item) => {
+    const price = item.fields.price;
+    if(price >= total) {
+      total = price;
+    }
+    return total
+  }, 0)/100;
+};
+
 const Index = () => {
-  const { products } = useFetch(url)
-  const [count, setCount] = useState(0)
+  const { products } = useFetch(url);
+  const [count, setCount] = useState(0);
+  // Here we will pass the cart right down to the SingleProduct component. Here everytime the cart value changes the function will render and it will run the addToCart function from scratch
+  const [cart, setCart] = useState(0);
+  // We will use the useCallback function in this case which will ensure that we only call the function if it's value has changed or else we won't call the function. So here if we click on the count then the cart won't re-render as we are not changing the cart value
+  const addToCart = useCallback(() => {
+    setCart(cart + 1);
+  }, [cart]);
+
+  // Here we are calling the calculateMostExpensive() function every time we are clicking on the button. Here as the useMemo() function will deal with the values we will pass the function that returns a value as the first argument to the useMemo() function and the second argument is the dependency array 
+  const mostExpensive = useMemo(() => calculateMostExpensive(products), [products]);
 
   return (
     <>
@@ -19,23 +39,25 @@ const Index = () => {
       <button className='btn' onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <BigList products={products} />
+      <h1 style={{marginTop: '3rem'}}>cart: {cart}</h1>
+      <h1>Most Expensive : ${mostExpensive}</h1>
+      <BigList products={products} addToCart={addToCart}/>
     </>
   )
 }
 
 // Here memo is our function and will wrap the logic of the component in the memo. Here the memo is memoizing the values i.e. if the prop value is not changed then we are not triggering the re-render 
-const BigList = React.memo(({ products }) => {
+const BigList = React.memo(({ products, addToCart }) => {
   return (
     <section className='products'>
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>
+        return <SingleProduct key={product.id} {...product} addToCart={addToCart}></SingleProduct>
       })}
     </section>
   )
 });
 
-const SingleProduct = ({ fields }) => {
+const SingleProduct = ({ fields, addToCart }) => {
   let { name, price } = fields
   price = price / 100
   const image = fields.image[0].url
@@ -45,6 +67,7 @@ const SingleProduct = ({ fields }) => {
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
+      <button onClick={addToCart}>add to cart</button>
     </article>
   )
 }
