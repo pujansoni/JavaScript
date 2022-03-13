a0a67f3881e25a139b3824dcbfb8b3b1f211eb13
Pujan Soni <pujansoni1997@gmail.com>
2022-03-13 19:01:32 -0400

    Color Generator values.js React/react-concept-projects

--- React/react-concept-projects/09-color-generator/setup/src/App.js
+++ React/react-concept-projects/09-color-generator/setup/src/App.js
@@ -3,8 +3,51 @@ import SingleColor from './SingleColor'
 
 import Values from 'values.js'
 
+// Here we will have a form where we will submit the form with the color value and if everything is correct then we will get a list of color values and iterate over those values to display them
+
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState('');
+  // We will also use the error useState as it will be used when the input is not a valid color. By default the value of error will be false
+  const [error, setError] = useState(false);
+  // Here we will have some values by default when we land on the page for the first time
+  // Here we will page the default values when the user land on the page for the first time
+  const [list, setList] = useState(new Values('#f15025').all(10));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Here we will check if the input is a valid color. If the input is not a valid color then we will have a red border around the input element and if it is a valid color then we will display the list
+
+    // Values.js
+    // Here in the values.js library we will have access to the Values as we have imported it above and we will pass in the color value and assign it to a variable. After that we will have access to different methods provided by the values.js library and we will use the all() method of the library which will give us the tints(lighter variant) and shades(darker variant) of that color. Here we will have "Unable to parse color from string" error if we have the invalid input for the color so we will wrap the logic in the try catch block to customize how to handle the error 
+    try {
+      // Here the argument 10 means that we are dividing 100% of tint(lighter variant) by 10 and 100% of shadow(darker variant) by 10. So more the value passed in the all() function less tint and shadow we get and vice-versa
+      let colors = new Values(color).all(10);
+      // setError(false);
+      setList(colors);
+    } catch(error) {
+      setError(true);
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={color} onChange={(e) => setColor(e.target.value )} placeholder="#f15025" className={`${error?'error': null}`} />
+          <button className="btn" type="submit">submit</button>
+        </form>
+      </section>
+      {/* In the section below we will iterate over the colors and display the list */}
+      <section className="colors">
+        {list.map((color, index) => {
+          console.log(color);
+          return <SingleColor key={index} {...color} index={index} hexColor={color.hex} />
+        })}
+      </section>
+    </>
+  );
 }
 
 export default App
--- React/react-concept-projects/09-color-generator/setup/src/SingleColor.js
+++ React/react-concept-projects/09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,35 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+// Here in the SingleColor we are looking for three things from the color object that we are deconstructing in the App.js
+// Here the rgb and weight are coming from the color object. Here we want the background to be same as the color in the rgb for the SingleColor component
+const SingleColor = ({rgb, weight, index ,hexColor}) => {
+  // Here we are setting up the alert useState cause we are also providing the functionality for the user to select a SingleColor component and copy the corresponding color to the clipboard and the alert message is used to indicate to the user that the color was copied to the clipboard
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(',');
+  // const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`;
+
+  // Here we need to clear the "copy to clipboard" text after 3 seconds
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return ()=>clearTimeout(timeout);
+  }, [alert]);
+  
+  // Here we are setting up the inline style for the background color. 
+  // Here we are also using the index from the prop in order to change the paragraphs color cause for the darker shades the weight and the hex values text color is not visible and we know that the all function from the values.js has the base color at index 10 so of any darker color variant the index is greater than 10 so we will add the color-light class for that component
+  return <article className={`color ${index > 10 && 'color-light'}`} style={{backgroundColor:`rgb(${bcg})`}} onClick={() => {
+    setAlert(true);
+    navigator.clipboard.writeText(hexValue);
+  }}>
+    <p className="percent-value">
+      {weight}%
+    </p>
+    <p className="color-value">{hexValue}</p>
+    {alert && <p className="alert">copied to clipboard</p>}
+  </article>
 }
 
 export default SingleColor
