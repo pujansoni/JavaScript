267696887bca9b5f6794a71187610ffc9509393a
Pujan Soni <pujansoni1997@gmail.com>
2022-03-15 22:40:38 -0400

    useRef Navbar React/react-concept-projects

--- React/react-concept-projects/11-navbar/setup/src/Navbar.js
+++ React/react-concept-projects/11-navbar/setup/src/Navbar.js
@@ -4,7 +4,55 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  const [showLinks, setShowLinks] = useState(false);
+  // Here we are using the useRef in order to dynamically set the value of the show-container class height and set the height equal to the height of the items present in the list dynamically
+  // This useRef is for the div
+  const linksContainerRef = useRef(null);
+  // This useRef is for the unordered list
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if(showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = '0px';
+    }
+  }, [showLinks]);
+
+  return <nav>
+    <div className="nav-center">
+      <div className="nav-header">
+        <img src={logo} alt="logo" />
+        <button className="nav-toggle" onClick={() => setShowLinks(!showLinks)}>
+          <FaBars />
+        </button>
+      </div>
+      {/* Here instead of using the conditional rendering to display the div we are using the ternary operator inside the div in order to add/remove the show-container class whose css has the correspondong transition property */}
+      <div className='links-container' ref={linksContainerRef}>
+        <ul className="links" ref={linksRef}>
+          {links.map((link) => {
+            const {id, url, text} = link;
+            return (
+              <li key={id}>
+                <a href={url}>{text}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <ul className="social-icons">
+        {social.map((socialIcon) => {
+          const {id, url, icon} = socialIcon;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  </nav>
 }
 
 export default Navbar
