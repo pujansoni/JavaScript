6e5d26735961a6b09a5f7e0b7eadb8d8cc3260ef
Pujan Soni <pujansoni1997@gmail.com>
2022-03-19 23:58:43 -0400

    Setting up Cocktail project React/react-concept-projects

--- React/react-concept-projects/15-cocktails/setup/src/App.js
+++ React/react-concept-projects/15-cocktails/setup/src/App.js
@@ -7,11 +7,27 @@ import SingleCocktail from './pages/SingleCocktail'
 import Error from './pages/Error'
 // import components
 import Navbar from './components/Navbar'
+// Here we need to wrap the router around the App() component. Alternatively we can also introduce the router around the App component in the index.js file as the router needs the context.js Provider details in order to process information so the router's code should be either between the Provider and the App component in index.js or around the App component in the App.js
+// Here the Navbar is above the Switch component as it's fixed
 function App() {
   return (
-    <div>
-      <h2>app component</h2>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Router exact path="/">
+          <Home />
+        </Router>
+        <Router path="/about">
+          <About />
+        </Router>
+        <Router path="/cocktail/:id">
+          <SingleCocktail />
+        </Router>
+        <Router path="*">
+          <Error />
+        </Router>
+      </Switch>
+    </Router>
   )
 }
 
