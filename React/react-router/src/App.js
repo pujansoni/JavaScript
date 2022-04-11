06614af5be1955ba4de4172dd51cf3fd2e0af8f2
Pujan Soni <pujansoni1997@gmail.com>
2022-04-10 21:12:03 -0400

    Adding the Link react element React/react-router

--- React/react-router/src/App.js
+++ React/react-router/src/App.js
@@ -1,5 +1,24 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Products from './pages/Products'
+import Error from './pages/Error';
+
 function App() {
-  return <h2>React Router 6 Tutorial</h2>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* The route below will be reached from localhost:3000/testing */}
+        {/* <Route path="testing" element={<div>
+          <h2>testing</h2>
+        </div>} /> */}
+        <Route path="about" element={<About />} />
+        <Route path="products" element={<Products />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
