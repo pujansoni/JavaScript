e165018c802a8190d2219652410799db838c3c77
Pujan Soni <pujansoni1997@gmail.com>
2022-04-16 01:21:32 -0400

    global axios instance React/axios-tutorial-react-main

--- React/axios-tutorial-react-main/src/examples/4-global-instance.js
+++ React/axios-tutorial-react-main/src/examples/4-global-instance.js
@@ -1,11 +1,19 @@
 import { useEffect } from 'react';
+// Using the global instance of the axios to setup some common parameters
+import axios from 'axios';
 
 const productsUrl = 'https://course-api.com/react-store-products';
 const randomUserUrl = 'https://randomuser.me/api';
 
 const GlobalInstance = () => {
   const fetchData = async () => {
-    console.log('global axios instance');
+    try {
+      // The downside of using the global instance is that it applies to all of the responses in axios and sometime we may want to avoid that e.g. when the response has sensitive data
+      const resp1 = await axios(productsUrl);
+      const resp2 = await axios(randomUserUrl);
+    } catch (error) {
+      
+    }
   };
 
   useEffect(() => {
