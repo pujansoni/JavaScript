1f9d6f769e61be5b280c8c917b55e60c99a12bf6
Pujan Soni <pujansoni1997@gmail.com>
2022-06-06 12:15:13 -0400

    Using app.use() for static files with express

--- NodeJS/node-express-course-main/02-express-tutorial/app.js
+++ NodeJS/node-express-course-main/02-express-tutorial/app.js
@@ -1,29 +1,22 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
-// OR
-// const express = require('express')();
+
+// Here we have kept all the static (static assets are files that server doesn't have to change) resources in the public folder i.e. logo, styles, and logic. We can now also navigate directly to the assets as well by typing in: http://localhost:5000/styles.css
+// Here we didn't have to take care of the statuses or the content types as express takes care of it
+// setup static and middleware [Note: app.use() is used to setup the middleware]
+app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
-    console.log('user hit the resource');
-    res.status(200).send('Home Page');
-});
-
-app.get('/about', (req, res) => {
-    res.status(200).send('About Page');
+    // Here we are using the absolute path
+    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
 });
 
 app.all('*', (req, res) => {
-    res.status(404).send('<h1>resource not found</h1>');
+    res.status(404).send('resource not found');
 });
 
 app.listen(5000, () => {
-    console.log('server is listening on port 5000...');
-})
-
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
+    console.log('sever is listening on port 5000....');
+});
